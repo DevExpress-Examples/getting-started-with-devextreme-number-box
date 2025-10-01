c49b346bb67b401b7b6cf6ee61aeff4317c074b6
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-10-01 13:43:20 +0300

    Update to 25.1.3+

---
+++ Vue/src/orig_App.vue
@@ -0,0 +1,35 @@
+<template>
+    <DxNumberBox
+        label="Enter a sum in dollars"
+        :label-mode="floating"
+        :value="261991"
+        format="$ #,##0.##"
+        :min="0"
+        :max="1000000"
+        :step="5"
+        :show-spin-buttons="true"
+        :show-clear-button="true"
+        @value-changed="onValueChanged($event)"
+    />
+</template>
+
+<script>
+import "devextreme/dist/css/dx.light.css";
+import { DxNumberBox } from "devextreme-vue/number-box";
+import notify from "devextreme/ui/notify";
+
+export default {
+    components: {
+        DxNumberBox
+    },
+    methods: {
+        onValueChanged(e) {
+            if (e.value) {
+                notify({
+                    message: "The sum is $" + e.value
+                });
+            }
+        }
+    }
+}
+</script>
