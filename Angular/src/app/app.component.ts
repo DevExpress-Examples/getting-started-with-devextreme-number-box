f55e822ed7a19e442261e674b6a85a4892642cba
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 11:37:16 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import notify from 'devextreme/ui/notify';
 import { DxNumberBoxTypes } from 'devextreme-angular/ui/number-box';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'Getting Started with DevExtreme Angular NumberBox';
